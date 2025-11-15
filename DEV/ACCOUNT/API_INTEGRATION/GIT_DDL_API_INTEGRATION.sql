562c4bdef842b6d74294ed87f363bdcc5de33ba7
agarwal194 <33786556+agarwal194@users.noreply.github.com>
2025-11-15 17:08:15 +0530

    Dev Deploy (auto-merge)
    Auto-merged by Snowflake inventory tool on 2025-11-15 11:38:14 UTC.

--- DEV/ACCOUNT/API_INTEGRATION/GIT_DDL_API_INTEGRATION.sql
+++ DEV/ACCOUNT/API_INTEGRATION/GIT_DDL_API_INTEGRATION.sql
@@ -1,5 +1,5 @@
 -- Snapshot for API INTEGRATION GIT_DDL_API_INTEGRATION (DESCRIBE API INTEGRATION)
--- Generated at: 2025-11-15 11:28:50 UTC
+-- Generated at: 2025-11-15 11:37:35 UTC
 -- Source SQL: DESC API INTEGRATION GIT_DDL_API_INTEGRATION
 
 -- property | property_type | property_value | property_default
